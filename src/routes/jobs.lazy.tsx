7ca6de230c897d5f7e9218e284d3f61c03707922
Manpreet Singh <manpreetmss400@gmail.com>
2024-06-25 12:40:11 +0530

    loader function  and userLoaderData hook for a fetch jobs

--- src/routes/jobs.lazy.tsx
+++ src/routes/jobs.lazy.tsx
@@ -1,40 +1,38 @@
 import JobCard from '@/components/JobCard';
-import { toast } from '@/components/ui/use-toast';
 import { getJobs } from '@/lib/apiClient';
-import { createLazyFileRoute } from '@tanstack/react-router';
-import { useEffect, useState } from 'react';
+import { createLazyFileRoute, useLoaderData } from '@tanstack/react-router';
 
 import type { JobItem } from '@/types/job.types';
+import { toast } from '@/components/ui/use-toast';
+
+export async function jobsLoader() {
+  try {
+    const res = await getJobs();
+    const jobs = res.data.data.jobs;
+    // console.log(" jobs ;;;;; ", jobs);
+    return jobs;
+  }
+  catch (error) {
+    toast({
+      title: "Error",
+      description: 'Failed to load jobs.',
+      variant: 'destructive'
+    });
+    console.error("err in fetching jobs ---> ", error);
+    return [];
+  }
+}
+
 
 export const Route = createLazyFileRoute('/jobs')({
   component: JobsPage,
+  loader: jobsLoader,
 });
 
 function JobsPage() {
-  const [jobs, setJobs] = useState<JobItem[]>();
+  const jobs = useLoaderData({ from: '/jobs' }) as JobItem[];
 
-  useEffect(() => {
-    async function getJobsHandler() {
-      try {
-        const res = await getJobs();
-        const jobs = res.data.data.jobs;
-        console.log(" jobs ;;;;; ", jobs);
-
-        setJobs(jobs as JobItem[]);
-      }
-      catch (error) {
-        toast({
-          title: "Error",
-          description: 'Failed to load jobs. Please try again.',
-          variant: 'destructive'
-        });
-      }
-    }
-
-    getJobsHandler();
-  }, []);
-
-  if (!jobs) return <h1>No Jobs here</h1>;
+  if (!jobs || !jobs.length) return <h1>No Jobs here</h1>;
 
   return (
     <div className='m-4'>
